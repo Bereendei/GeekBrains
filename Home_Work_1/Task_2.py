ffb102d25a74658b52f2e500a2518ef54b1ee060
Bereendei <sidorovivan1996@gmail.com>
2021-11-08 16:16:48 +0300

    ПЗ 2

--- Home_Work_1/Task_2.py
+++ Home_Work_1/Task_2.py
@@ -10,9 +10,9 @@ minutes = (user_time - hour * 3600) // 60
 second = user_time - (hour * 3600 + minutes * 60)
 
 if hour < 24:
-    print(f'Время в формате чч:мм:сс  {hour}:{minutes}:{second}')
+    print(f'Время в формате чч:мм:сс  {hour:02}:{minutes:02}:{second:02}')
 elif (hour > 24) and (hour < 7300):
     day = hour // 24
-    print(f'Кажется нужно добавить дни  {day}:{hour}:{minutes}:{second}')
+    print(f'Кажется нужно добавить дни  {day:02}:{hour:02}:{minutes:02}:{second:02}')
 else:
     print('Ох, походу тут еще и месяца, а то и года... Мне лень! Cам считай!')
